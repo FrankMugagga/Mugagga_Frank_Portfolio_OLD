57c65ac5290bd7eaf5a354c70c412742c956e8fe
FrankMugagga <frank.mugagga@gmail.com>
2023-02-07 17:08:58 +0300

    added portfolio folder

---
+++ portfolio.js
@@ -0,0 +1,29 @@
+const click = document.querySelector('#try');
+
+
+click.addEventListener('click', ()=>{ alert('we') });
+/*
+const button = document.querySelector('.sidebut');
+const menu = document.querySelector('.menu_nav');
+const exit_menu = document.querySelector('.exit');
+
+
+function open(){
+    sidebut.style.visibility = 'hidden';
+    menu.style.display = 'block';
+    menu.classList.toggle(open);
+    exit_menu.style.visibility = 'visible';
+}
+
+function close(){
+    sidebut.style.visibility = 'visible';
+    menu.style.display = 'none';
+    menu.classList.remove(close);
+    exit_menu.style.visibility = 'hidden';
+}
+
+
+button.addEventListener('click', open);
+menu.addEventListener('click', close);
+exit_menu.addEventListener('click', close);
+*/
