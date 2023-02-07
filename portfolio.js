8ce8d5d14a94b8ec9a428e5454f2434908b0af59
FrankMugagga <frank.mugagga@gmail.com>
2023-02-07 18:20:57 +0300

    added menu

--- portfolio.js
+++ portfolio.js
@@ -1,29 +1,11 @@
-const click = document.querySelector('#try');
+/*const hamburger = document.querySelector('hamburger');
+const navmenu = document.querySelector('hamburger');
 
+hamburger.addEventListener('click', () =>{
+   hamburger.classList.toggle('active');
+   navmenu.classList.toggle('active');
+})*/
 
-click.addEventListener('click', ()=>{ alert('we') });
-/*
-const button = document.querySelector('.sidebut');
-const menu = document.querySelector('.menu_nav');
-const exit_menu = document.querySelector('.exit');
+const sidebut = document.querySelector('.sidebut');
 
-
-function open(){
-    sidebut.style.visibility = 'hidden';
-    menu.style.display = 'block';
-    menu.classList.toggle(open);
-    exit_menu.style.visibility = 'visible';
-}
-
-function close(){
-    sidebut.style.visibility = 'visible';
-    menu.style.display = 'none';
-    menu.classList.remove(close);
-    exit_menu.style.visibility = 'hidden';
-}
-
-
-button.addEventListener('click', open);
-menu.addEventListener('click', close);
-exit_menu.addEventListener('click', close);
-*/
+sidebut.addEventListener('click', () => sidebut.classList.toggle('active'));
